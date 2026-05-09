587eaf0e9c8d09a0a5ed35c39c77d16ee0596978
kissthermite <kissthermite+github.com@protonmail.com>
2026-05-09 18:13:31 +0700

    Change allowed apps to excluded in HookEntry

--- app/src/main/java/io/github/brucezhang1993/warp_loves_play_store/HookEntry.kt
+++ app/src/main/java/io/github/brucezhang1993/warp_loves_play_store/HookEntry.kt
@@ -9,12 +9,12 @@ import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
 
 @InjectYukiHookWithXposed
 object HookEntry : IYukiHookXposedInit {
-    private val allowed = setOf("com.android.vending","com.google.android.gms","com.google.android.youtube","com.google.android.apps.photos")
+    private val excluded = setOf("com.cloudflare.onedotonedotonedotone","com.cloudflare.cloudflareoneagent")
     override fun onInit() = configs { isDebug = BuildConfig.DEBUG }
     override fun onHook() = YukiHookAPI.encase {
         listOf("com.cloudflare.onedotonedotonedotone","com.cloudflare.cloudflareoneagent").forEach { pkg ->
             loadApp(pkg) {
-                "android.net.VpnService\$Builder".toClass().method { name = "addDisallowedApplication"; param(StringClass); returnType = "android.net.VpnService\$Builder".toClass() }.hook { before { val p = args().first().string(); if (p in allowed) { result = instanceOrNull; return@before }; result = callOriginal() } }
+                "android.net.VpnService\$Builder".toClass().method { name = "addDisallowedApplication"; param(StringClass); returnType = "android.net.VpnService\$Builder".toClass() }.hook { before { val p = args().first().string(); if (p !in excluded) { result = instanceOrNull; return@before }; result = callOriginal() } }
             }
         }
     }
